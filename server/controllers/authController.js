fa45c0e47157525efe23a220667d04c70996cd6c
Fideltodayy <fidelotieno11@gmail.com>
2024-07-05 13:29:54 +0300

    receive jwt on registration

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -26,9 +26,24 @@ export const register = async (req, res) => {
       role,
     });
 
-    res
-      .status(201)
-      .json({ message: "User created successfully", user: newUser });
+    const token = jwt.sign(
+      { id: newUser.user_id, role: newUser.role },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: newUser.user_id,
+        username: newUser.username,
+        email: newUser.email,
+        role: newUser.role,
+      },
+      token,
+    });
   } catch (error) {
     console.error("Error during registration:", error);
     res.status(500).json({ message: "Server error", error });
